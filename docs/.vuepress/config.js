3b632345032286e464975b67344bdf60db320897
psp <976477689@qq.com>
2022-10-11 15:21:36 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    title: '秘密花园',
+    description: '距离百年前端还差98年',
+    theme: 'vuepress-theme-maker',
+    themeConfig: {
+        logo: 'https://80shuo.com/logo.jpg',
+        nav: [
+            { text: '首页', link: '/' },
+            { text: '关于我', link: '/about/' }
+        ],
+        social: [
+            {
+                type: 'email',
+                link: '976477689@qq.com'
+            }
+        ]
+    }
+}
